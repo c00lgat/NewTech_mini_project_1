8e950089263b04c779dfe38d41ffeaf149039105
Anan Abed <a28_a3@yahoo.com>
2026-01-04 20:33:56 +0200

    Changed from AmazonLinux AMI to Ubuntu 24.04 AMI

--- terraform/modules/ec2_launch_template/user_data.sh
+++ terraform/modules/ec2_launch_template/user_data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "hello"
+dnf update && dnf upgrade -y 
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
--- terraform/modules/ec2_launch_template/variables.tf
+++ terraform/modules/ec2_launch_template/variables.tf
@@ -1,6 +1,11 @@
 variable "amazon_linux_ami_id" {
   type    = string
-  default = "ami-08d7aabbb50c2c24e"
+
+  #Amazon Linux AMI
+  # default = "ami-08d7aabbb50c2c24e"
+
+  #Ubuntu LTS 24.04 AMI
+  default = "ami-0ecb62995f68bb549"
 }
 
 variable "key_location" {
