1336234404862366e5b88b8a64030ed01783bd1d
Anan Abed <a28_a3@yahoo.com>
2026-01-05 15:12:27 +0200

    changes to user_data.sh and added deployment job

--- terraform/modules/ec2_launch_template/user_data.sh
+++ terraform/modules/ec2_launch_template/user_data.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+
+mkdir /flask-app
+
+
+cat > /flask-app/docker-compose.yml <<'EOF'
+services:
+  app:
+    image: ${IMAGE_URI}
+    ports:
+      - "5000:5000"
+    restart: unless-stopped
+EOF
