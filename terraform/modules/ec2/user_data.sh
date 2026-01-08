5b921ceacbd0ea9cb1ee23fb2633b439cfb251cb
Anan Abed <a28_a3@yahoo.com>
2026-01-08 15:19:32 +0200

    Deleted unnecessary EC2 module

--- terraform/modules/ec2/user_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get upgrade -y
-apt-get install python3.11 python3-pip git
-
-adduser app
-mkdir app
-
-git clone https://github.com/no3am/NewTech_DevOps_Nov25/tree/main/mini-project1
